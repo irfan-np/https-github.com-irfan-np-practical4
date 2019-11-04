21647e0eed37fe5b05782e1467e62d0e74e769eb
nazurah <s10189240@connect.np.edu.sg>
2019-11-04 17:57:17 +0800

    practical4

---
+++ Telegramme/AddContactViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddContactViewController.swift
+//  Telegramme
+//
+//  Created by MAD2_P02 on 4/11/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
--- Telegramme/ViewController.swift
+++ Telegramme/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var usrNameFld: UITextField!
+    @IBOutlet weak var pwdFld: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginBtn(_ sender: Any) {
+        if true { // if username and password is correct
+            
+            let storyboard = UIStoryboard(name: "Content", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "Content") as UIViewController
+            vc.modalPresentationStyle = .fullScreen // try without fullscreen
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    
 }
 
